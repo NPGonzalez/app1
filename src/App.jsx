bb7ba0682db6be9dc62b185ec876cf5cd139aa0e
NPGonzalez <naypili@gmail.com>
2020-09-02 22:01:50 -0400

    Se agrega botón y se le agrega estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './App.css';
 
 class App  extends React.Component {
     name = 'Pili';
     render(){
             return (
                 <div className = "App" >
+                    <button>Botón 1</button>
                   {this.name}
                 </div>
     );
