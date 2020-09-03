0365d083ed576df7aa27f14c12d303cc225b75bf
NPGonzalez <naypili@gmail.com>
2020-09-02 23:11:53 -0400

    Se agrega estilo de div dentro del componente

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,16 @@ import './App.css';
 class App  extends React.Component {
     name = 'Pili';
     render(){
+        const styles = {
+            border: 'solid',
+            textAlign: 'center',
+            boxshadow: '2px 2px'
+        }
             return (
                 <div className = "App" >
+                    <div style={styles}>
+                        Styles
+                    </div>
                     <button>Botón 1</button>
                   {this.name}
                 </div>
