8ae6f2d1ae22e63622054c1ed0d2d3b3670b3b85
NPGonzalez <naypili@gmail.com>
2020-09-02 23:26:26 -0400

    Se mueve constante de estilo a archivo .jsx se exporta desde allí y se importa en en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css';
+import * as styles from './styles';
 
 class App  extends React.Component {
     name = 'Pili';
     render(){
-        const styles = {
-            border: 'solid',
-            textAlign: 'center',
-            boxshadow: '2px 2px'
-        }
+        // const styles = {
+        //     border: 'solid',
+        //     textAlign: 'center',
+        //     boxshadow: '2px 2px'
+        // }
             return (
                 <div className = "App" >
-                    <div style={styles}>
+                    <div style={styles.styles}>
                         Styles
                     </div>
                     <button>Botón 1</button>
---
+++ src/styles.jsx
@@ -0,0 +1,5 @@
+      export  const  styles = {
+            border: 'solid',
+            textAlign: 'center',
+            boxshadow: '2px 2px'
+        }
